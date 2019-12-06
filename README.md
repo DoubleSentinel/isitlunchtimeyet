6470cb9c443fb789d245096edbf3ff5064315789
Nicolas Huguenin <7804729+ymaktepi@users.noreply.github.com>
2019-12-06 10:01:22 +0100

    Feat/docker (#5)
    * feat: moved application in app folder
    
    * feat: WIP docker
    
    * feat: docker
    
    * feat: added restart always
    
    * feat: add small readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Is it lunchtime yet?
+
+Find out using this app!
+
+## Run locally
+
+- `pipenv install`
+
+- `FLASK_APP=app/app.py FLASK_ENV=development pipenv run flask run`
+
+## Run with docker
+
+- `cp docker-compose.example.yml docker-compose.yml`
+- Change config in `docker-compose.yml`
+- `docker-compose up`
+OR
+- Run defaults: `docker-compose -f docker-compose.example.yml up`
--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template 
+from flask import Flask, request, render_template
 from datetime import datetime, time
 import urllib, json, pytz
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 URL_FOR_API = 'http://ip-api.com/json/'
 
 LUNCH_HOURS = { 'start': time(hour=11, minute=30),
-                'end': time(hour=13)} 
+                'end': time(hour=13)}
 
 def get_client_ip_location(request):
     if app.debug:
@@ -15,17 +15,19 @@ def get_client_ip_location(request):
         client_ip =  '72.229.28.185' # New York
     else:
         client_ip = request.remote_addr
-    req = urllib.request.Request(URL_FOR_API + client_ip) 
+    req = urllib.request.Request(URL_FOR_API + client_ip)
     return json.loads(urllib.request.urlopen(req).read())
 
 @app.route('/', methods=['GET'])
 def web():
-    client_timezone = get_client_ip_location(request)['timezone'] 
+    client_timezone = get_client_ip_location(request)['timezone']
     client_time_now = datetime.now(pytz.timezone(client_timezone)).time()
-    its_lunchtime = LUNCH_HOURS['start'] <= client_time_now < LUNCH_HOURS['end'] 
+    its_lunchtime = LUNCH_HOURS['start'] <= client_time_now < LUNCH_HOURS['end']
     return render_template('template.html',
                             its_lunchtime=its_lunchtime)
 
 @app.route('/api')
 def api():
     return
+
+print("lskdjflkjd")
