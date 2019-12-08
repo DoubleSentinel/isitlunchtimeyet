9c34ffb57b710cc6c89f801e0b70afc7cf8a2437
Nicolas Huguenin <nichuguen@gmail.com>
2019-12-08 18:32:07 +0100

    ci(lint): check that flake fails ci on lint

--- app/app.py
+++ app/app.py
@@ -7,17 +7,21 @@ app = Flask(__name__)
 
 URL_FOR_API = 'http://ip-api.com/json/'
 
-LUNCH_HOURS = { 'start': time(hour=11, minute=30),
-                'end': time(hour=13)}
+LUNCH_HOURS = {
+    'start': time(hour=11, minute=30),
+    'end': time(hour=13)
+}
+
 
 def get_client_ip_location(request):
     if app.debug:
         # client_ip = '144.2.103.184' # Neuchâtel
-        client_ip =  '72.229.28.185' # New York
+        client_ip = '72.229.28.185'  # New York
     else:
         client_ip = request.remote_addr
     response = http_get(URL_FOR_API + client_ip)
     return response.json()
+
 
 def is_lunchtime(request):
     try:
@@ -25,13 +29,14 @@ def is_lunchtime(request):
         client_timezone = client_info['timezone']
         client_time_now = datetime.now(pytz.timezone(client_timezone)).time()
         return LUNCH_HOURS['start'] <= client_time_now < LUNCH_HOURS['end']
-    except KeyError as e:
+    except KeyError:
         raise BaseException("Could not find timezone from IP", client_info['message'])
+
 
 @app.route('/', methods=['GET'])
 def web():
-    its_lunchtime = is_lunchtime(request)
     return render_template('index.html')
+
 
 @app.route('/api')
 def api():
